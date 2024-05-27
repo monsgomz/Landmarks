20c2cda674c5a18c1017f7b81a6dc64c99871c40
M Montserrat Gomez <gome0145@algonquinlive.com>
2024-05-27 12:51:39 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Montserrat Gomez on 2024-05-27.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
