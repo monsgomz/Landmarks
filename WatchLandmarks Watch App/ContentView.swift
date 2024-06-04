6a8027d2c6671979ba9fb8d38f2312600e7933c0
Montserrat Gomez <montserrat.ga95@gmail.com>
2024-06-03 18:04:24 -0600

    Add watchOS app detail view

---
+++ WatchLandmarks Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Montserrat Gomez on 2024-06-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
